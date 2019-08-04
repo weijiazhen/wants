a9ca36227453d0e3e2c4128859168e849d021e54
zyy <1348844913@qq.com>
2019-08-04 19:12:00 +0800

    zhengyi1

--- src/vants/index.js
+++ src/vants/index.js
@@ -10,11 +10,31 @@ import {
     TabbarItem,
     //图标引入
     Icon,
+    //导航栏
+    NavBar,
+    //输入框
+    Field,
+    // 按钮
+    Button,
+    // 单元格
+    Cell,
+    CellGroup,
 } from 'vant';
+
 
 export default () => {
     Vue.use(Tabbar);
     Vue.use(TabbarItem);
     Vue.use(Search);
     Vue.use(Icon);
+    //导航栏
+    Vue.use(NavBar);
+    // 输入框
+    Vue.use(Field);
+    // 按钮
+    Vue.use(Button);
+    // 单元格
+    Vue.use(Cell).use(CellGroup);
+
+
 }
